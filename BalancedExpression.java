4544a885862f0120dc6968bb53fedcbba4cb9d86
Rumana Aktar <33204625+rumana-aktar@users.noreply.github.com>
2017-11-27 12:27:01 -0400

    Add files via upload

---
+++ BalancedExpression.java
@@ -0,0 +1,46 @@
+/*
+ * check whether en expression is balanced or not: idea geeksforgeeks
+ */
+package balancedexpression;
+import java.util.*;
+/**
+ *
+ * @author rumana aktar
+ */
+public class BalancedExpression {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static boolean balancedExpression(String exp){
+        Stack<Character> stack=new Stack<>();
+        
+        for(int i=0; i<exp.length(); i++){
+            char ch=exp.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='[')
+                stack.push(ch);
+            else if(ch==')'){
+                if(stack.pop()!='(')
+                    return false;
+            }else if(ch=='}'){
+                if(stack.pop()!='{')
+                    return false;
+            }else if(ch==']'){
+                if(stack.pop()!='[')
+                    return false;
+            }            
+        }
+        if(stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print(balancedExpression("()(({})[])"));
+        System.out.print("\n"+balancedExpression("()(({]})[])"));
+    }
+    
+}
